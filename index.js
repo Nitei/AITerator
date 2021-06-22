0df2022481763a503da8cea738bab9963aec314b
Jorge <jorgecampoy81@gmail.com>
2021-06-22 22:51:19 +0200

    version 1.2.0

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const AITerator = require( './dist/AITerator' );
+// const AITerator = require( './dist/AITerator' );
+import { AITerator } from "./dist/AITerator.d.ts";
+
 module.exports = AITerator;
